8d115936c89cefdcb5af5b7c3d314b3e6bd40ae3
Predrag Icić <icicpredrag@gmail.com>
2026-08-17 00:46:20 +0200

    Initial book_service.py - edit_book()

--- services/book_service.py
+++ services/book_service.py
@@ -29,11 +29,32 @@ def add_book():
     save_json("storage/books.json", books)
 
 def edit_book():
-    pass
+    books = load_json("storage/books.json")
+    for book in books:
+        print(f"{book['isbn']}. {book['title']} by {book['author']}")
+    book_isbn = input("Enter the book ISBN to edit: ")
+    for book in books:
+        if book["isbn"] == book_isbn:
+            fields = {k: v for k, v in book.items() if k != "isbn"}
+            for i, (k, v) in enumerate(fields.items(), start=1):
+                print(f"{i}. {k}: {v}")
+            choice = int(input("Enter the field you want to edit: "))
+            edit_function = {
+                "title": get_title,
+                "author": get_author,
+                "year": get_year,
+                "total_copies": get_total_copies,
+            }
+            field = list(fields.keys())[choice - 1]
+            get_value = edit_function[field]
+            new_value = get_value()
+            book[field] = new_value
+            save_json("storage/books.json", books)
+            print("=" * 20)
 
 def delete_book():
     pass
 
 
 if __name__ == "__main__":
-    add_book()
+    edit_book()
