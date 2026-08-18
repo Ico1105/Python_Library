ff3050847488f56d72763bddbb93e19e0bb89ba8
Predrag Icić <icicpredrag@gmail.com>
2026-08-18 22:24:14 +0200

    Initial rental_service.py, rent_book(), rental.py

--- models/rental.py
+++ models/rental.py
@@ -8,3 +8,23 @@ class Rental:
         self.return_date = return_date
 
 
+    def to_dict(self):
+        return {
+            "rental_id": self.rental_id,
+            "member_id": self.member_id,
+            "isbn": self.isbn,
+            "rental_date": self.rental_date,
+            "due_date": self.due_date,
+            "return_date": self.return_date,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            rental_id=data["rental_id"],
+            member_id=data["member_id"],
+            isbn=data["isbn"],
+            rental_date=data["rental_date"],
+            due_date=data["due_date"],
+            return_date=data["return_date"],
+        )
--- services/rental_service.py
+++ services/rental_service.py
@@ -1,8 +1,78 @@
-def generate_rental_id():
-    pass
+from datetime import date, timedelta
+
+from json_storage import load_json, save_json
+from models.rental import Rental
+
+
+def generate_rental_id(rentals):
+    if not rentals:
+        return 1
+
+    return max(rental["rental_id"] for rental in rentals) + 1
 
 def rent_book():
-    pass
+    members = load_json("storage/members.json")
+    books = load_json("storage/books.json")
+    rentals = load_json("storage/rentals.json")
+
+    # Find member
+    for member in members:
+        print(
+              f"{member['member_id']}."
+              f"{member['first_name']} {member['last_name']}"
+        )
+    member_id = int(input("Enter the member ID: "))
+    member = None
+
+    for item in members:
+        if item["member_id"] == member_id:
+            member = item
+            break
+
+    if member is None:
+        print("Member not found.")
+        return
+
+    # Find books
+    for book in books:
+        print(
+            f"{book['book_id']}."
+            f"{book['title']} "
+            f"(Available: {book['available_copies']})"
+        )
+    isbn = input("Enter the book ISBN: ")
+    # Fing book
+    book = None
+    for item in books:
+        if item["isbn"] == isbn:
+            book = item
+            break
+    if book is None:
+        print("Book not found.")
+        return
+    # Check availability
+    if book["available_copies"] <= 0:
+        print("Book is not available.")
+        return
+
+    # Create rental
+    rental_id = generate_rental_id(rentals)
+    rental_date = date.today()
+    due_date = rental_date + timedelta(days=14)
+    rental = Rental(
+        rental_id=rental_id,
+        member_id=member_id,
+        isbn=isbn,
+        rental_date=rental_date.isoformat(),
+        due_date=due_date.isoformat(),
+        return_date=None,
+    )
+    book['available_copies'] -= 1
+    rentals.append(rental.to_dict())
+    save_json("storage/rentals.json", rentals)
+    save_json("storage/books.json", books)
+
+    print("Book rented successfully.")
 
 def return_book():
     pass
